bb845d3e17585708794e49bccfa28a63e678b1da
Katie Hawcutt <katiehawcutt@gmail.com>
2020-08-08 13:03:37 +0100

    refactor to reduce repetition and fix local storage

--- main.js
+++ main.js
@@ -1,80 +1,47 @@
-if (localStorage.storedLifeList) {
-  loadLists();
-}
-if (localStorage.storedWorkList) {
-  loadLists();
-}
-if (localStorage.storedShoppingList) {
-  loadLists();
+const lists = [
+  { title: "Life Tasks", id: "lifeJobsList", items: [] },
+  { title: "Works Tasks", id: "workJobsList", items: [] },
+  { title: "Shopping", id: "shoppingList", items: [] },
+];
+
+function addTask(listId, inputId, listIndex) {
+  var text = document.getElementById(inputId).value;
+  displayAndSaveTask(listId, text, listIndex);
+  document.getElementById(inputId).value = "";
 }
 
-function addLifeTask() {
+function displayAndSaveTask(listId, text, listIndex) {
+  lists[listIndex].items.push(text);
   var newItem = document.createElement("div");
-  newItem.innerHTML = document.getElementById("lifeJobsItem").value;
-  newItem.onclick = removeLifeTask;
-  document.getElementById("lifeJobsList").appendChild(newItem);
-  document.getElementById("lifeJobsItem").value = "";
+  newItem.innerHTML = text;
+  newItem.onclick = function () {
+    removeTask(listId, newItem, listIndex);
+  };
+  document.getElementById(listId).appendChild(newItem);
   saveLists();
 }
 
-function addWorkTask() {
-  var newItem = document.createElement("div");
-  newItem.innerHTML = document.getElementById("workJobsItem").value;
-  newItem.onclick = removeWorkTask;
-  document.getElementById("workJobsList").appendChild(newItem);
-  document.getElementById("workJobsItem").value = "";
+function removeTask(listId, newItem, listIndex) {
+  var text = newItem.innerHTML;
+  var itemIndex = lists[listIndex].items.indexOf(text);
+  lists[listIndex].items.splice(itemIndex, 1);
+  document.getElementById(listId).removeChild(newItem);
   saveLists();
 }
-function addShoppingItem() {
-  var newItem = document.createElement("div");
-  newItem.innerHTML = document.getElementById("shoppingListItem").value;
-  newItem.onclick = removeShoppingItem;
-  document.getElementById("shoppingList").appendChild(newItem);
-  document.getElementById("shoppingListItem").value = "";
-  saveLists();
-}
-
-function removeLifeTask() {
-  document.querySelector("#lifeJobsList").removeChild(this);
-  saveLists();
-}
-function removeWorkTask() {
-  document.querySelector("#workJobsList").removeChild(this);
-  saveLists();
-}
-function removeShoppingItem() {
-  document.querySelector("#shoppingList").removeChild(this);
-  saveLists();
-}
-
-//TODO: refactor with querySelectorAll
 
 function saveLists() {
-  localStorage.storedLifeList = document.querySelector(
-    "#lifeJobsList"
-  ).innerHTML;
-  localStorage.storedWorkList = document.querySelector(
-    "#workJobsList"
-  ).innerHTML;
-  localStorage.storedShoppingList = document.querySelector(
-    "#shoppingList"
-  ).innerHTML;
+  localStorage.setItem("savedLists", JSON.stringify(lists));
 }
 
 function loadLists() {
-  document.querySelector("#lifeJobsList").innerHTML =
-    localStorage.storedLifeList;
-  for (var i = 0; i < lifeJobsList.children.length; i++) {
-    lifeJobsList.children[i].onclick = removeLifeTask;
-  }
-  document.querySelector("#workJobsList").innerHTML =
-    localStorage.storedWorkList;
-  for (var i = 0; i < workJobsList.children.length; i++) {
-    workJobsList.children[i].onclick = removeWorkTask;
-  }
-  document.querySelector("#shoppingList").innerHTML =
-    localStorage.storedShoppingList;
-  for (var i = 0; i < shoppingList.children.length; i++) {
-    shoppingList.children[i].onclick = removeShoppingItem;
-  }
+  var jsonList = localStorage.getItem("savedLists");
+  var savedLists = JSON.parse(jsonList);
+  savedLists.forEach((list, listIndex) => {
+    list.items.forEach((item) => {
+      if (list.id) {
+        displayAndSaveTask(list.id, item, listIndex);
+      }
+    });
+  });
 }
+loadLists();
